fb681ffe6c68204d8a597c84e46004fed9a85dc7
Asifur <asifurr38@gmail.com>
2026-01-06 23:02:47 +0600

    fourteenth

--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,10 @@
 function loginValidate(){
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
+
+    if(user == "" || pass == ""){
+        document.getElementById("error").innerHTML = "All fields are required!";
+        return false;
+    }
+    return true;
 }
