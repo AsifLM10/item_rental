4e274bf2deeb3a3a0ff481d373043fcbe758010c
Asifur <asifurr38@gmail.com>
2026-01-07 00:34:47 +0600

    seventeenth

--- control/login.php
+++ control/login.php
@@ -4,7 +4,10 @@ session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $user = $_POST["username"];
     $pass = $_POST["password"];
-    
+
+    if(empty($user) || empty($pass)){
+        echo "All fields required";
+    }
 }
 
 ?>
