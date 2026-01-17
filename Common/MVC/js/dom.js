37c5cf66607b0992f1a59db730ec849b18a3586c
Asifur <asifurr38@gmail.com>
2026-01-18 02:21:35 +0600

    65

--- Common/MVC/js/dom.js
+++ Common/MVC/js/dom.js
@@ -1,4 +1,11 @@
 function toggleMode(){
     var body = document.body;
     var text = document.getElementById("modeText");
+
+    if(body.style.backgroundColor == "black"){
+        body.style.backgroundColor = "";
+        body.style.color = "black";
+        text.innerHTML = "Light Mode";
+    }
+    
 }
