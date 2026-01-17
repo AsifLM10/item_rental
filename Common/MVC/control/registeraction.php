f6a568513e458a73c5101dfd18e8e52dee97a042
Asifur <asifurr38@gmail.com>
2026-01-18 02:15:43 +0600

    61

--- Common/MVC/control/registeraction.php
+++ Common/MVC/control/registeraction.php
@@ -9,3 +9,13 @@ if(empty($user) || empty($pass) || empty($role)){
     echo "All fields required";
     exit();
 }
+
+$hash = password_hash($pass, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users(username,password,role) VALUES('$user','$hash','$role')";
+
+if(mysqli_query($conn,$sql)){
+    echo "Registration Successful <a href='../view/login.php'>Login</a>";
+}
+
+?>
