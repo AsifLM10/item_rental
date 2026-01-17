f96d45cd8f4c2f34f8bfd138c8c30793357e7a4e
Asifur <asifurr38@gmail.com>
2026-01-18 01:22:05 +0600

    46

--- Common/view/index.php
+++ Common/view/index.php
@@ -9,5 +9,19 @@ session_start();
         <link rel="stylesheet" href="../css/style.css">
         <script src="../js/dom.js"></script>
     </head>
-    
+    <body>
+        <?php include "header.php"; ?>
+
+        <div class="top">
+            <h1>Item Rental Management System</h1>
+            <p>Manage and rent items easily with a simple and secure platform.</p>
+
+            <?php
+            if(!isset($_SESSION["username"])){
+                echo "<a href='login.php' class='btn'>Get Started</a>";
+            }
+            ?>
+        </div>
+    </body>
+
 </html>
