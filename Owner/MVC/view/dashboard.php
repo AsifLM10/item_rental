6278a1c8f2daced3ec0c50e2c95b5e99dd02bb1e
Asifur <asifurr38@gmail.com>
2026-01-18 03:11:03 +0600

    76

--- Owner/MVC/view/dashboard.php
+++ Owner/MVC/view/dashboard.php
@@ -11,4 +11,12 @@ if(!isset($_SESSION["username"]) || $_SESSION["role"] != "owner"){
         <title>Owner Dashboard</title>
         <link rel="stylesheet" href="../../../Common/MVC/css/style.css">
     </head>
+    <body>
+        <?php include "../../../Common/MVC/view/header.php"; ?>
+
+        <div class="section">
+            <h2>Owner Dashboard</h2>
+            <p>Welcome <?php echo $_SESSION["username"]; ?></p>
+        </div>
+    </body>
 </html>
