658acb342240f125918ad0451bf72b6f9c7fecfe
Asifur <asifurr38@gmail.com>
2026-01-18 03:36:23 +0600

    86

--- Owner/MVC/control/itemAction.php
+++ Owner/MVC/control/itemAction.php
@@ -3,4 +3,20 @@ session_start();
 include "../../../Common/MVC/database/config.php";
 
 $action = $_POST["action"] ?? "";
+
+if($action == "add"){
+    $item = $_POST["item_name"];
+    $price = $_POST["price"];
+    $owner = $_SESSION["username"];
+
+    if(empty($item) || empty($price)){
+        echo "ALl fields required";
+        exit();
+    }
+
+    $sql = "INSERT INTO items(item_name, price, status, owner_username) VALUES ('$item','$price','Available','$owner')";
+
+    mysqli_query($conn, $sql);
+    header("Location: ../view/manageItems.php");
+}
 ?>
