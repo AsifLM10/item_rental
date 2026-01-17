ff2f77dcdfcf7a470e847179ac79fc0d341b80d8
Asifur <asifurr38@gmail.com>
2026-01-18 03:05:02 +0600

    74

--- Owner/MVC/view/dashboard.php
+++ Owner/MVC/view/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(!isset($_SESSION["username"]) || $_SESSION["role"] != "owner"){
+    header("Location: ../../../Common/MVC/view/login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
     
