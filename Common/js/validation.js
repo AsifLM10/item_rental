6ad6370567580672d61fe5d5475e2e926f10e625
Asifur <asifurr38@gmail.com>
2026-01-18 01:04:10 +0600

    41

--- Common/js/validation.js
+++ Common/js/validation.js
@@ -2,5 +2,9 @@ function loginValidate(){
     var u = document.getElementById("username").value;
     var p = document.getElementById("password").value;
 
-    
+    if(u == "" || p == ""){
+        document.getElementById("error").innerHTML = "All fields required";
+        return false;
+    }
+    return true;
 }
