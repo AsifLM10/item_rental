00cc3589dba7c84e10a2c323f8580076537026e6
Asifur <asifurr38@gmail.com>
2026-01-18 00:57:18 +0600

    37

--- Common/view/index.php
+++ Common/view/index.php
@@ -5,6 +5,9 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <title>Item Rental Management System</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="../js/dom.js"></script>
     </head>
+    
 </html>
