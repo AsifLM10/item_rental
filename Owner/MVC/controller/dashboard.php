38e89fa34b1c0dccf3c8b27806648244718998b6
Asifur <asifurr38@gmail.com>
2026-01-19 00:07:13 +0600

    106

--- Owner/MVC/controller/dashboard.php
+++ Owner/MVC/controller/dashboard.php
@@ -2,8 +2,9 @@
 session_start();
 
 if(!isset($_SESSION["username"]) || $_SESSION["role"] !== "owner"){
-    header("Location: ../../../Common/MVC/view/login.php")
+    header("Location: ../../../Common/MVC/view/login.php");
     exit();
+    
 }
 
 $displayName = $_SESSION["username"];
