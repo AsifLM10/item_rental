f388459b8becf0c82deeecef6282aeb3f57f1693
Asifur <asifurr38@gmail.com>
2026-01-19 03:38:53 +0600

    139

--- Owner/MVC/controller/deleteItems.php
+++ Owner/MVC/controller/deleteItems.php
@@ -1,3 +1,9 @@
 <?php
 //session_start();
+include("../../../Common/MVC/database/config.php");
+
+if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "owner") {
+    header("Location: ../../../Common/MVC/view/login.php");
+    exit();
+}
 ?>
