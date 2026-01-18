4bc62fb8c0837f4208f1f229143be19c5a033dda
Asifur <asifurr38@gmail.com>
2026-01-19 04:56:26 +0600

    149

---
+++ Owner/MVC/javascript/manageItems.js
@@ -0,0 +1,6 @@
+function openEdit(id, name, description, price){
+    document.getElementById("edit_id").value = id;
+    document.getElementById("edit_name").value = name;
+    document.getElementById("edit_description").value = description;
+    document.getElementById("edit_price").value = price;
+}
