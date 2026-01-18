755e9f60c7812a999cc1be8782f705001e8f1676
Asifur <asifurr38@gmail.com>
2026-01-19 01:04:38 +0600

    115

--- Owner/MVC/controller/addItem.php
+++ Owner/MVC/controller/addItem.php
@@ -1,3 +1,34 @@
 <?php
 session_start();
+include("../../../Common/MVC/database/config.php");
+
+if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "owner") {
+    header("Location: ../../../Common/MVC/view/login.php");
+    exit();
+}
+
+$message = "";
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $name=trim($_POST["name"]);
+    $description = trim($_POST["description"]);
+    $price = (int)$_POST["price"];
+
+    if($name && $description && $quantity > 0){
+        $owner=$_SESSION["username"];
+        $status = "available";
+        $sql = "INSERT INTO items(item_name,description,price,status,owner_username) VALUES('$name','$description','$price','$status','$owner')";
+
+        if(mysqli_query($conn,$sql)){
+            $message="Item added successfully!";
+        } 
+        else{
+            $message = "Error";
+        }
+    }
+    else{
+        $message = "All fields are required";
+    }
+
+}
 ?>
