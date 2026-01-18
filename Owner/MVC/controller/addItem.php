527205bc7dacd2487f592ef7e1de6255b43db409
Asifur <asifurr38@gmail.com>
2026-01-19 02:25:18 +0600

    127

--- Owner/MVC/controller/addItem.php
+++ Owner/MVC/controller/addItem.php
@@ -1,5 +1,7 @@
 <?php
-//session_start();
+if(session_start() === PHP_SESSION_NONE){
+    session_start();
+}
 include("../../../Common/MVC/database/config.php");
 
 if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "owner") {
@@ -17,13 +19,13 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $imageName=$_FILES["image"]["name"];
     $tmpName=$_FILES["image"]["tmp_name"];
 
-    $uploadDir = "../uploads/";
+    $uploadDir = "../../../uploads/";
     $newImageName=time() . "_" . basename($imageName);
     $uploadPath=$uploadDir . $newImageName;
 
 
 if(move_uploaded_file($tmpName, $uploadPath)){
-    if($name && $description && $quantity > 0){
+    if($name && $description && $price > 0){
         $owner=$_SESSION["username"];
         $status = "available";
         $sql = "INSERT INTO items(item_name,description,price,status,owner_username) VALUES('$name','$description','$price','$status','$owner')";
