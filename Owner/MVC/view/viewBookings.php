6a38a492dcb3d167e91b1d66d158e637310342f5
Asifur <asifurr38@gmail.com>
2026-01-19 06:35:15 +0600

    161

--- Owner/MVC/view/viewBookings.php
+++ Owner/MVC/view/viewBookings.php
@@ -15,7 +15,26 @@ include("../controller/viewBookings.php");
         <h2 class="title">Booking Requests</h2>
 
         <div class="container">
-            
+            <?php if(mysqli_num_rows($result) > 0): ?>
+                <table>
+                    <tr>
+                        <th>Item</th>
+                        <th>Renter</th>
+                        <th>Price</th>
+                        <th>Status</th>
+                    </tr>
+
+                    <?php while($row=mysqli_fetch_assoc($result)): ?>
+                        <tr>
+                            <td><?php echo $row["item_name"]; ?></td>
+                            <td><?php echo $row["renter_username"]; ?></td>
+                            <td>৳<?php echo $row["price"]; ?></td>
+                            <td><?php echo $row["request_status"]; ?></td>
+                        </tr>
+                        <?php endwhile; ?>
+                </table>
+                <?php else: ?>
+                    <p class="empty">No booking requests yet.</p>
         </div>
     </body>
 </html>
