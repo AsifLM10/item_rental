20b8c7ba47ed2cfaf49800f3f0852f6504b33893
Asifur <asifurr38@gmail.com>
2026-01-19 07:22:08 +0600

    172

--- Owner/MVC/view/profile.php
+++ Owner/MVC/view/profile.php
@@ -9,6 +9,30 @@ include "../controller/profile.php";
     <link rel="stylesheet" href="../stylesheets/profile.css">
 </head>
 <body>
+    <a href="dashboard.php" class="home-btn">Dashboard</a>
 
+<div class="container">
+    <div class="card">
+        <h2>Edit Profile</h2>
+
+        <?php if (!empty($message)): ?>
+            <p class="msg"><?php echo $message; ?></p>
+        <?php endif; ?>
+
+        <form method="post">
+            <label>Username</label>
+            <input type="text" value="<?php echo $_SESSION["username"]; ?>" disabled>
+
+            <label>Email</label>
+            <input type="email" name="email"
+                   value="<?php echo $user["email"]; ?>" required>
+
+            <label>New Password (leave blank to keep current)</label>
+            <input type="password" name="password">
+
+            <button type="submit">Update Profile</button>
+        </form>
+    </div>
+</div>
 </body>
 </html>
