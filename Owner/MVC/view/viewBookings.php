bcea4219eb92ac5fce6d58515f859d3c3c9c893d
Asifur <asifurr38@gmail.com>
2026-01-19 06:28:54 +0600

    160

--- Owner/MVC/view/viewBookings.php
+++ Owner/MVC/view/viewBookings.php
@@ -1,3 +1,21 @@
 <?php 
 include("../controller/viewBookings.php"); 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View Bookings</title>
+        <link rel="stylesheet" href="../stylesheets/viewBookings.css">
+    </head>
+
+    <body>
+        <a href="dashboard.php" class="home-btn">Dashboard</a>
+
+        <h2 class="title">Booking Requests</h2>
+
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
